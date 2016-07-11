2ffbf6eb79929e54deed706fd84ab8e3e74bf79c
Tony Kelman <tony@kelman.net>
2016-07-11 15:06:39 -0700

    Use conda instead of conda-forge, and get openssl and zlib
    on linux where they're needed
    
    todo: add a test that runs inside a minimal centos docker container

--- deps/build.jl
+++ deps/build.jl
@@ -4,6 +4,23 @@ if is_apple()
     using Homebrew
 end
 
+function download_and_unpack(baseurl, filename)
+    downloadurl = baseurl * filename
+    info("Downloading $filename from $downloadurl\nTo avoid this " *
+        "download, install git manually and add it to your path " *
+        "before\nrunning Pkg.add(\"Git\") or Pkg.build(\"Git\")")
+    dest = joinpath("usr", string(Sys.MACHINE))
+    for dir in ("downloads", "usr", dest)
+        isdir(dir) || mkdir(dir)
+    end
+    filename = joinpath("downloads", filename)
+    isfile(filename) || run(download_cmd(downloadurl, filename))
+    # TODO: checksum validation
+    (b, ext, sec_ext) = splittarpath(filename)
+    run(unpack_cmd(filename, dest, is_windows() ? ".7z" : ext, sec_ext))
+    # TODO: make this less noisy on windows, see how WinRPM does it
+end
+
 gitcmd = `git`
 gitver = "notfound"
 try
@@ -16,40 +33,30 @@ if gitver == "notfound"
         error("Working git not found on path, try running\nPkg.build(\"Homebrew\")")
     end
     baseurl = ""
-    filename = ""
-    if is_linux() && Sys.ARCH === :x86_64
-        # use conda for a non-root option
-        gitver = "2.8.2"
-        baseurl = "http://anaconda.org/conda-forge/git/$gitver/download/linux-64/"
-        filename = "git-$gitver-2.tar.bz2"
-    elseif is_linux() && (Sys.ARCH in (:i686, :i586, :i486, :i386))
-        # conda-forge doesn't build for 32 bit linux
+    if is_linux() && (Sys.ARCH in (:x86_64, :i686, :i586, :i486, :i386))
+        # use conda for a non-root option on x86/amd64 linux
+        # TODO? use conda-forge when we no longer build julia on centos 5
         gitver = "2.6.4"
-        baseurl = "http://anaconda.org/anaconda/git/$gitver/download/linux-32/"
-        filename = "git-$gitver-0.tar.bz2"
+        plat = "download/linux-$(Sys.WORD_SIZE)/"
+        baseurl = "http://anaconda.org/anaconda/git/$gitver/$plat"
+        download_and_unpack(baseurl, "git-$gitver-0.tar.bz2")
+        # dependencies for linux
+        sslver = "1.0.2h"
+        sslbase = "http://anaconda.org/anaconda/openssl/$sslver/$plat"
+        download_and_unpack(sslbase, "openssl-$sslver-1.tar.bz2")
+        zlibver = "1.2.8"
+        zlibbase = "http://anaconda.org/anaconda/zlib/$zlibver/$plat"
+        download_and_unpack(zlibbase, "zlib-$zlibver-3.tar.bz2")
     elseif is_windows()
         # download and extract portablegit
         gitver = "2.9.0"
-        baseurl = "https://github.com/git-for-windows/git/releases/" *
-            "download/v$gitver.windows.1/"
-        filename = "PortableGit-$gitver-$(Sys.WORD_SIZE)-bit.7z.exe"
+        baseurl = "https://github.com/git-for-windows/git/releases/download/"
+        download_and_unpack(baseurl * "v$gitver.windows.1/",
+            "PortableGit-$gitver-$(Sys.WORD_SIZE)-bit.7z.exe")
     end
-    downloadurl = baseurl * filename
-    if !isempty(downloadurl)
-        info("Downloading git version $gitver from $downloadurl\nTo " *
-            "avoid this download, install git manually and add it to your " *
-            "path before\nrunning Pkg.add(\"Git\") or Pkg.build(\"Git\")")
-        dest = joinpath("usr", string(Sys.MACHINE))
-        for dir in ("downloads", "usr", dest)
-            isdir(dir) || mkdir(dir)
-        end
-        filename = joinpath("downloads", filename)
-        isfile(filename) || run(download_cmd(downloadurl, filename))
-        # TODO: checksum validation
-        (b, ext, sec_ext) = splittarpath(filename)
-        run(unpack_cmd(filename, dest, is_windows() ? ".7z" : ext, sec_ext))
-        # TODO: make this less noisy on windows, see how WinRPM does it
-        gitcmd = `$(joinpath(dirname(@__FILE__), dest, "bin", "git"))`
+    if !isempty(baseurl)
+        gitpath = joinpath(dirname(@__FILE__), "usr", string(Sys.MACHINE), "bin", "git")
+        gitcmd = `$gitpath`
     end
     try
         gitver = readchomp(`$gitcmd --version`)
@@ -58,8 +65,8 @@ if gitver == "notfound"
         # by setting an environment variable in deps.jl
     catch err
         error("Could not automatically install git, error was: $err\n" *
-            (is_windows() ? "" : "Try installing git via your " *
-            "system package manager then running\nPkg.build(\"Git\")"))
+            (is_windows() ? "Report an issue at https://github.com/JuliaPackaging/Git.jl/issues/new" :
+            "Try installing git via your system package manager then running\nPkg.build(\"Git\")"))
     end
 else
     try
