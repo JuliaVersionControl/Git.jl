321d746010cbc93978fdb5899d3b4840f893e7fb
Mosè Giordano <mose@gnu.org>
2023-02-16 23:25:13 +0000

    Add work around for SIP restrictions on macOS

--- test/runtests.jl
+++ test/runtests.jl
@@ -43,3 +43,22 @@ end
     @test orig_cainfo == get_env("GIT_SSL_CAINFO")
     @test orig_templatedir == get_env("GIT_TEMPLATE_DIR")
 end
+
+# This makes sure the work around for the SIP restrictions on macOS
+# (<https://github.com/JuliaVersionControl/Git.jl/issues/40>) works correctly.  While SIP is
+# a macOS-specific issue, it's good to exercise this code path everywhere.
+@testset "SIP workaround" begin
+    gitd(dir, cmd) = run(`$(git()) -C $(dir) -c "user.name=a" -c "user.email=b@c" $(cmd)`)
+    branch = "dev"
+    mktempdir() do dir1; mktempdir() do dir2;
+        gitd(dir1, `init --bare --quiet --initial-branch $(branch)`)
+        gitd(dir2, `init --quiet --initial-branch $(branch)`)
+        open(joinpath(dir2, "README"); write=true) do io
+            println(io, "test")
+        end
+        gitd(dir2, `add --all`)
+        gitd(dir2, `commit --quiet -m test`)
+        gitd(dir2, `remote add origin file://$(dir1)`)
+        gitd(dir2, `push --quiet --set-upstream origin $(branch)`)
+    end; end
+end
