981401a8f9086546fd52b1aea7ca33131bec5931
Morten Piibeleht <morten.piibeleht@juliahub.com>
2025-07-24 14:09:34 +0300

    refactor

--- src/git_function.jl
+++ src/git_function.jl
@@ -22,9 +22,12 @@ julia> run(git(["clone", "https://github.com/JuliaRegistries/General"]))
 to bypass the parsing of the command string.
 """
 function git(; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
-    git_cmd = @static if Sys.iswindows()
-        Git_jll.git(; adjust_PATH, adjust_LIBPATH)::Cmd
-    else
+    git_cmd = Git_jll.git(; adjust_PATH, adjust_LIBPATH)::Cmd
+    env_mapping = Dict{String,String}(
+        "PATH" => _get_cmd_env(git_cmd, "PATH"),
+        LIBPATH_env => _get_cmd_env(git_cmd, LIBPATH_env),
+    )
+    @static if !Sys.iswindows()
         root = Git_jll.artifact_dir
 
         libexec = joinpath(root, "libexec")
@@ -36,7 +39,6 @@ function git(; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
 
         ssl_cert = joinpath(dirname(Sys.BINDIR), "share", "julia", "cert.pem")
 
-        env_mapping = Dict{String,String}()
         env_mapping["GIT_EXEC_PATH"]    = libexec_git_core
         env_mapping["GIT_SSL_CAINFO"]   = ssl_cert
         env_mapping["GIT_TEMPLATE_DIR"] = share_git_core_templates
@@ -47,15 +49,12 @@ function git(; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
             # more details.
             env_mapping["JLL_DYLD_FALLBACK_LIBRARY_PATH"] = Git_jll.LIBPATH[]
         end
-
-        original_cmd = Git_jll.git(; adjust_PATH, adjust_LIBPATH)::Cmd
-        addenv(original_cmd, env_mapping...)::Cmd
     end
 
     # Use OpenSSH from the JLL: <https://github.com/JuliaVersionControl/Git.jl/issues/51>.
     if !Sys.iswindows() && OpenSSH_jll.is_available()
-        path = split(get(ENV, "PATH", ""), pathsep)
-        libpath = split(get(ENV, LIBPATH_env, ""), pathsep)
+        path = split(get(env_mapping, "PATH", ""), pathsep)
+        libpath = split(get(env_mapping, LIBPATH_env, ""), pathsep)
 
         path = vcat(dirname(OpenSSH_jll.ssh_path), path)
         libpath = vcat(OpenSSH_jll.LIBPATH_list, libpath)
@@ -65,27 +64,36 @@ function git(; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
         unique!(filter!(!isempty, path))
         unique!(filter!(!isempty, libpath))
 
-        git_cmd = addenv(git_cmd, "PATH" => join(path, pathsep), LIBPATH_env => join(libpath, pathsep))
+        env_mapping["PATH"] = join(path, pathsep)
+        env_mapping[LIBPATH_env] = join(libpath, pathsep)
     end
 
     # Add git-lfs
     if Git_LFS_jll.is_available()
-        # Read path from git_cmd.env as it can be modified above
-        idx = findfirst(startswith("PATH="), git_cmd.env)
-        path = if isnothing(idx)
-            ""
-        else
-            # dropping the `PATH=` part
-            git_cmd.env[idx][6:end]
-        end
-        path = vcat(dirname(Git_LFS_jll.git_lfs_path), path)
-        git_cmd = addenv(git_cmd, "PATH" => join(path, pathsep))::Cmd
+        env_mapping["PATH"] = string(
+            dirname(Git_LFS_jll.git_lfs_path),
+            pathsep,
+            get(env_mapping, "PATH", "")
+        )
     end
-    return git_cmd
+
+    return addenv(git_cmd, env_mapping...)::Cmd
 end
 
 function git(args::AbstractVector{<:AbstractString}; kwargs...)
     cmd = git(; kwargs...)
     append!(cmd.exec, args)
     return cmd
+end
+
+# The .env field of a Cmd object is an array of strings in the format
+# `$(key)=$(value)` for each environment variable.
+function _get_cmd_env(cmd::Cmd, key::AbstractString)
+    idx = findfirst(startswith("$(key)="), cmd.env)
+    if isnothing(idx)
+        return ""
+    else
+        # dropping the `$(key)=` part
+        return cmd.env[idx][(ncodeunits(key)+2):end]
+    end
 end
